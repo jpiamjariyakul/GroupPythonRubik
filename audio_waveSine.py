e3d92d1bf30098ccdb87965fda11a65d089dad8f
Jay Piamjariyakul <j.piamjariyakul@outlook.com>
2020-02-21 23:49:52 +0000

    Minor audits to audio generation process
    Issue: frequency output to audio has half the intended frequency - investigate

--- audio_waveSine.py
+++ audio_waveSine.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.signal
 import pygame, pygame.sndarray
+from matplotlib import pyplot as plot
 
 # Reference:  	http://shallowsky.com/blog/programming/python-play-chords.html
 #				https://docs.scipy.org/doc/numpy/reference/generated/numpy.sin.html
@@ -13,12 +14,15 @@ def playAudio(frqc, ms):
 		# Compute N samples of a sine wave with given frequency and peak amplitude
 		# Number of samples per second == sample rate == 44.1k Hz
 		length = sample_rate / float(hz) # Sets sample length given sample rate
-		omega = np.pi * 2 / length # Obtain 
-		t = np.arange(int(length)) * omega
+		w = (np.pi * 2) / length # Obtain 
+		t = np.arange(int(length)) * w
 		onecycle = amplitude * np.sin(t)
-		return (np.resize(onecycle, (sample_rate,)).astype(np.int16))
+		waveFull = np.resize(onecycle, sample_rate).astype(np.int16)
+		return waveFull
 	# Converts array into sound (via make_sound())
+	#print(len(sine_wave(frqc)))
 	sound = pygame.sndarray.make_sound(sine_wave(frqc))
+	print("Frqc: " + str(frqc))
 	sound.play(-1) # Plays the sound
 	pygame.time.delay(ms) # Pause for a given number of milliseconds
 	sound.stop() # Stops the sound
@@ -26,14 +30,14 @@ def playAudio(frqc, ms):
 def audioPlayMove(frqcCurrent):
 	duration = int(50) # Duration of audio in ms
 	pygame.mixer.init(channels=1) # Sets audio to output in channel 1
-	playAudio(frqcCurrent, duration)
+	playAudio((9 * 1000), duration) # 2 added ad hoc since output frqc is half intended
 
 def audioInputSeq(ls_moves):# Receives list of moves
 	dict_frqcFilter = { # All of these values are in kHz
 		# 'U': 1.55303,	'R': 2.28267,
 		# 'F': 0.5034,	'D': 0.05034,
 		# 'L': 0.15247,	'B': 1.0707
-		'U': 00.5,	'R': 04.5,
+		'U': 00.5,	'R': 03.5,
 		'F': 08.5,	'D': 12.5,
 		'L': 16.5,	'B': 20.5	}
 	print(ls_moves)
@@ -43,4 +47,4 @@ def audioInputSeq(ls_moves):# Receives list of moves
 		print("Playing audio for " + str(frqcCurrent) + " Hz")
 		for turn in range(frqcCurrent[1]): audioPlayMove(frqcCurrent[0]) # Plays audio corresponding to moves in list
 
-#audioInputSeq(['U'])
+audioInputSeq([('F', 1)])
